c4a0d567195a1690684fe29bc3b7c65348905baa
Ashish Kumar <ashishkr45943@gmail.com>
2026-02-15 19:21:14 +0530

    disturduted the auth path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom"
-import AuthPage from './pages/auth'
+import SignupPage from './pages/signupPage'
+import LoginPage from './pages/loginPage'
 import Dashboard from './pages/dashboard'
 import Landing from './pages/landingPage'
 import NotFound from './pages/notFound'
@@ -10,7 +11,8 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Landing />} />
-      <Route path='/auth' element={<AuthPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/signup' element={<SignupPage />} />
       <Route path='/dashboard' element={<Dashboard />} />
       <Route path='*' element={<NotFound />} />
 
