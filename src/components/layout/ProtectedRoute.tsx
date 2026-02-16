c801fa6a655b1257fd8edbf0cf37d6e1a9d9dcdb
Ashish Kumar <ashishkr45943@gmail.com>
2026-02-16 14:17:48 +0530

    added unauth users rollback login in here

---
+++ src/components/layout/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { useAppSelector } from '../../app/hooks'
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { isAuthenticated } = useAppSelector((state) => state.auth)
+  const location = useLocation()
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />
+  }
+
+  return <>{children}</>
+};
+
+export default ProtectedRoute
